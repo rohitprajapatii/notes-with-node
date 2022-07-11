65d08f83de6658feb501b7baa0f20ecb35c9538a
rohitprajapatii <rohitpra.d16@gmail.com>
2022-07-11 18:55:07 +0530

    Integrated with the mongo atlas

--- app.js
+++ app.js
@@ -1,13 +1,28 @@
 const express = require("express");
 const morgan = require("morgan");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const notes = require("./routes/notes");
 const app = express();
 
 app.use(morgan("dev"));
+
+mongoose
+  .connect(
+    "mongodb+srv://Rohit123:" +
+      "Rohit123" +
+      "@cluster0.kihun.mongodb.net/?retryWrites=true&w=majority",
+    {
+      // useMongoClient: true,
+    }
+  )
+  .catch((err) => {
+    console.log(err);
+  });
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const notes = require("./routes/notes");
 app.use("/notes", notes);
 
 app.use((req, res, next) => {
--- routes/notes.js
+++ routes/notes.js
@@ -1,39 +1,122 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const Notes = require("../models/notes");
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    message: "Handling GET requests to /notes",
-  });
+  Notes.find()
+    .select("_id title content")
+    .then((notes) => {
+      const response = {
+        count: notes.length,
+        notes: notes,
+      };
+      res.status(200).json(response);
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "Fetching notes failed!",
+        error: err,
+      });
+    });
+});
+
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Notes.findById(id)
+    .select("_id title content")
+    .then((note) => {
+      if (note) {
+        res.status(200).json(note);
+      } else {
+        res.status(404).json({ message: "Note not found" });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.post("/create", (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
-  res.status(201).json({
-    message: "Handling POST requests to /createNote with title " + title,
+  const newNote = new Notes({
+    _id: mongoose.Types.ObjectId(),
+    title: title,
+    content: content,
   });
-});
-
-router.get("/:id", (req, res, next) => {
-  const id = req.params.id;
-  res.status(200).json({
-    message: "Handling GET requests to /notes/" + id,
-  });
+  newNote
+    .save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Note created successfully",
+        createdNote: {
+          _id: result._id,
+          title: result.title,
+          content: result.content,
+        },
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.patch("/:id", (req, res, next) => {
   const id = req.params.id;
-  res.status(200).json({
-    message: "Handling PATCH requests to /notes/" + id,
+  const body = req.body;
+  const reqOps = {};
+  body.forEach((el) => {
+    reqOps[el.name] = el.value;
   });
+
+  Notes.findById(id)
+    .then((note) => {
+      if (!note) {
+        res.status(404).json({
+          message: "Note not found",
+        });
+        return;
+      } else {
+        Notes.updateOne({ _id: id }, { $set: reqOps })
+          .exec()
+          .then((result) => {
+            console.log(result);
+            res.status(200).json({
+              message: "Note updated successfully",
+            });
+          });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 router.delete("/:id", (req, res, next) => {
   const id = req.params.id;
-  res.status(200).json({
-    message: "Handling DELETE requests to /notes/" + id,
-  });
+  Notes.deleteOne({ _id: id })
+    .exec()
+    .then((_) => {
+      res.status(200).json({
+        message: "Note deleted successfully",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: err,
+      });
+    });
 });
 
 module.exports = router;
